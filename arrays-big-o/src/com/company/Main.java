9e648752e2b24ad5d5e4f617a331176702516574
BhaviniB <lassiebhavini@gmail.com>
2018-06-26 23:22:16 +0530

    Arrays reviewed

---
+++ arrays-big-o/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] intArray=new int[7];
+	    intArray[0]=1;
+        intArray[1]=2;
+        intArray[2]=4;
+        intArray[3]=-99;
+        intArray[4]=10;
+        intArray[5]=100;
+        intArray[6]=-33;
+
+
+        for(int i=0;i<intArray.length;i++)
+        {
+            System.out.println(intArray[i]);
+        }
+
+    }
+}
