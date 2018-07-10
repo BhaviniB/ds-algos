6226df8c0537692aad76d052e340f6c8d7aef284
BhaviniB <lassiebhavini@gmail.com>
2018-07-11 00:13:52 +0530

    class Employee created

---
+++ array-list/src/com/company/Employee.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) { 
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
