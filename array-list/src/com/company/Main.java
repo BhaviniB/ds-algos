93ee5853b834e4022c31aaf38e3205318d4ae68f
BhaviniB <lassiebhavini@gmail.com>
2018-07-11 00:36:36 +0530

    arraylist created using the list interface

--- array-list/src/com/company/Main.java
+++ array-list/src/com/company/Main.java
@@ -8,7 +8,18 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         List<Employee> employeeList=new ArrayList<>();
-        
+        employeeList.add(new Employee("Jughead","Jones",22));
+        employeeList.add(new Employee("Betty","Cooper",22));
+        employeeList.add(new Employee("Veronica","Lodge",22));
+        employeeList.add(new Employee("Archie","Andrews",22));
+        employeeList.forEach(employee -> System.out.println(employee));//lambda expr
+        System.out.println(employeeList.get(1)); //const time
+
+
+        System.out.print(employeeList.isEmpty());
+
+
+
 
 
     }
