571ed4be853c0e8756694e238391a61dec832299
BhaviniB <lassiebhavini@gmail.com>
2018-07-11 00:52:09 +0530

    to Array()

--- array-list/src/com/company/Main.java
+++ array-list/src/com/company/Main.java
@@ -21,7 +21,16 @@ public class Main {
         employeeList.forEach(employee -> System.out.println(employee));//lambda expr
 
         System.out.println(employeeList.size());
-        
+        //adding and default shifting
+
+        employeeList.add(3,new Employee("Fred","Andrews",44));
+        employeeList.forEach(employee -> System.out.println(employee));//lambda expr
+        Employee [] employeeArray=employeeList.toArray(new Employee[employeeList.size()]);
+        for(Employee employee:employeeArray){
+            System.out.print(employee);
+        }
+
+
 
 
 
